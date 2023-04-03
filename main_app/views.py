8851a0f92d374d282768b89ef0e258f9a0c87c6d
Leo <leonardovrodriguez246@gmail.com>
2023-04-03 10:20:16 -0400

    added basic navbar routes and views w/ context

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
+from .models import Recipe
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', 
+        {'recipe':recipe}
+    )
+
+def create_recipe(request):
+    return render(request, 'create_recipe.html')
+
+def meal_plan(request):
+    return render(request, 'meal_plan.html')
+
+def recipes(request):
+    return render(request, 'recipes.html')
+
+def recipes_detail(request):
+    return render(request, 'detail.html',{
+        'recipe': recipe,
+        'ingredient': ingredient
+    })
