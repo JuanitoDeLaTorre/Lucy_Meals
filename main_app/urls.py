fdf74797f958583f6f39f11b5510ab71f14d65dc
Leo <leonardovrodriguez246@gmail.com>
2023-04-03 10:41:43 -0400

    fixed views bug and added contex

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create_recipe/', views.create_recipe, name='create_recipe'),
     path('meal_plan/',views.meal_plan, name='meal_plan'),
     path('recipes/', views.recipes, name='recipes'),
-    path('recipe/<int:recipe_id>/', views.recipes_detail, 'detail'),
+    path('recipe/<int:recipe_id>/', views.recipes_detail, name='detail'),
 ]
