55bca57bfec11a65eab16325edf0c6ef9621a428
Leo <leonardovrodriguez246@gmail.com>
2023-04-03 09:50:15 -0400

    added navbar and extended setup

--- Lucy/settings.py
+++ Lucy/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'Lucy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lucy_shopping',
     }
 }
 
