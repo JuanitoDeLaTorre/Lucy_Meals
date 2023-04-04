5c559eb11b8c86aa21540c62c752c31a66c0db5b
JuanitoDeLaTorre <home.john.thomas@gmail.com>
2023-04-04 18:52:39 -0400

    changed models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient
+from .models import Recipe, Ingredient, MealPlan
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
+admin.site.register(MealPlan)
 # Register your models here.
