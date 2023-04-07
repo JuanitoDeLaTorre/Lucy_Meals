eccc17c10c299356750bbbeabcc554c88690d45a
Leo <leonardovrodriguez246@gmail.com>
2023-04-07 11:14:36 -0400

    added customUserCreationForm

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,5 +1,11 @@
 from django.forms import ModelForm
 from .models import Ingredient, Recipe
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.forms.fields import EmailField
+from django.forms.forms import Form
 
 class IngredientForm(ModelForm):
   class Meta:
@@ -10,3 +16,41 @@ class RecipeForm(ModelForm):
   class Meta:
     model = Recipe
     fields = ['name', 'category', 'day_of_week', 'img_url']
+
+    
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(label='username', min_length=5, max_length=150)
+    email = forms.EmailField(label='email')
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirm password', widget=forms.PasswordInput)
+
+    def username_clean(self):
+        username = self.cleaned_data['username'].lower()
+        new = User.objects.filter(username=username)
+        if new.count():
+            raise ValidationError("User Already Exist")
+        return username
+
+    def email_clean(self):
+        email = self.cleaned_data['email'].lower()
+        new = User.objects.filter(email=email)
+        if new.count():
+            raise ValidationError(" Email Already Exist")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        return user
